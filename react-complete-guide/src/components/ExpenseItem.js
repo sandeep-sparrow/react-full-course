56af3483a12c4a68c52423e79beb25b5bc67702a
Sandeep Prajapati <sandeep.p4856@gmail.com>
2023-09-03 00:20:06 +0530

    added reusable cards component

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,5 +1,6 @@
-import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
+import Card from './Card';
+import './ExpenseItem.css';
 
 function ExpenseItem(props){
 
@@ -8,13 +9,13 @@ function ExpenseItem(props){
     const expenseTitle = props.expense.title;
 
     return (
-    <div className="expense-item">
-        <ExpenseDate date={expenseDate}/>       
-        <div className="extense-item__description">
-            <h2>{expenseTitle}</h2>
-        </div>
-        <div className="expense-item__price">${expenseAmount}</div>
-    </div>
+        <Card className="expense-item">
+            <ExpenseDate date={expenseDate}/>       
+            <div className="extense-item__description">
+                <h2>{expenseTitle}</h2>
+            </div>
+            <div className="expense-item__price">${expenseAmount}</div>
+        </Card>
     );
 
 }
