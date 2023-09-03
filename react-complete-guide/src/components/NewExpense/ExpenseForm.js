3de3ef28acaef68e76f55cab6e953f0026ac6367
Sandeep Prajapati <sandeep.p4856@gmail.com>
2023-09-04 01:06:20 +0530

    added Event Handling and getting user Input to build new Expense Data

--- react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -1,25 +1,74 @@
-import React from "react";
+import React, {useState} from "react";
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+
+    // Example of Shared Handler
+
+    // const inputChangeHandler = (identifier, value) => {
+    //     if(identifier === "title"){
+    //         setEnteredTitle(value);
+    //     }else if(identifier === "date"){
+    //         setEnteredDate(value);
+    //     }else{
+    //         setEnteredAmount(value);
+    //     }
+    // };
+
+    const titleChangeHandler = (event) => {
+        // console.log(event.target.value);
+        setEnteredTitle(event.target.value);
+    };
+
+    const amountChangeHandler = (event) => {
+        // console.log(event.target.value);
+        setEnteredAmount(event.target.value);
+    };
+
+    const dateChangeHandler = (event) => {
+        // console.log(event.target.value);
+        setEnteredDate(event.target.value);
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+
+        console.log(expenseData);
+        // 2 way binding - very important when working with forms in react...
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    };
+
     return (
         <form>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text"/>
+                    {/* <input type="text" onChange={(event) => inputChangeHandler("title", event.target.value)}/> */}
+                    <input type="text" value={enteredTitle} onChange={titleChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01"/>
+                    <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2023-12-31"/>
+                    <input type="date" min="2019-01-01" max="2023-12-31" value={enteredDate} onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div className="new-expense__actions">
-                <button type="submit">Add Expense</button>
+                <button type="submit" onClick={submitHandler}>Add Expense</button>
             </div>
         </form>
     );
